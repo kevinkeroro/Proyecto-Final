5f768d499c42bb6405be1a22a083cbb817158c25
Kevin Limaylla Cusi <kevin_keroro@hotmail.com>
2022-05-21 15:45:43 +0200

    basic functionallity

---
+++ src/Entity/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
