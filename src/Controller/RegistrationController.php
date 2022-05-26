53eecd224bd0f801df11edb08260499ffc8d77c1
Kevin Limaylla Cusi <kevin_keroro@hotmail.com>
2022-05-26 13:36:03 +0200

    update login and logout front

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,13 +45,16 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('admin@topphone.com', 'Mail Bot'))
+                    ->from('admin@admin.com')
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
+
+
+            //FIXME: NO SE ENVIA EL EMAIL 
             // do anything else you need here, like send an email
             $this->addFlash('success', 'Verify your email address');
             return $this->redirectToRoute('app_login');
